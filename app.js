db26acdd702adc615f5161807af67775e2671048
Haiyun wang <why7121@gmail.com>
2020-06-20 12:12:50 +1000

    update media query

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ var year1 = document.getElementsByClassName('year')[0];
 var year2 = document.getElementsByClassName('year')[1];
 var info1 = document.getElementsByClassName('info')[0];
 var info2 = document.getElementsByClassName('info')[1];
-var credit = document.querySelector('.credit');
 var Icon1 = document.getElementsByClassName('skillIcons')[0];
 var Icon2 = document.getElementsByClassName('skillIcons')[1];
 var Icon3 = document.getElementsByClassName('skillIcons')[2];
 var Icon4 = document.getElementsByClassName('skillIcons')[3];
+var d6 = document.getElementById('section6');
+var title = document.querySelector('.title');
+var message1 = document.getElementsByClassName('contact-message')[0];
+var message2 = document.getElementsByClassName('contact-message')[1];
 var viewportHeight =window.innerHeight;
 var viewportWidth = window.innerWidth;
 var d2Top = viewportHeight;
 var d3Top = 6*viewportHeight;
 var d4Top = 8*viewportHeight;
 var d5Top = 10*viewportHeight;
+var d6Top = 12*viewportHeight;
 //resize event
 window.onresize = resize;
 function resize(){
@@ -31,21 +35,25 @@ window.addEventListener('scroll',function(){
     var h = window.scrollY;
     photo.style.opacity = updateStyle(h,0,viewportHeight,10,100,'%');
     photo.style.borderRadius = updateStyle(h,viewportHeight,2*viewportHeight,0,50,'%');
-    photo.style.width = updateStyle(h,viewportHeight,2*viewportHeight,50,25,'vw');
-    photo.style.transform = `translateX(${updateStyle(h,2*viewportHeight,3*viewportHeight,0,60,'%')})`;
+    photo.style.width = updateStyle(h,viewportHeight,2*viewportHeight,100,35,'vh');
+    photo.style.height = updateStyle(h,viewportHeight,2*viewportHeight,100,35,'vh');
+    photo.style.transform = `translateX(${updateStyle(h,2*viewportHeight,3*viewportHeight,0,-80,'%')})`;
     introduction.style.opacity = updateStyle(h,2*viewportHeight,3*viewportHeight,0,100,'%');
-    year1.style.transform = `translateX(${updateStyle(h,4*viewportHeight,6*viewportHeight,-200,0,'%')})`;
-    info1.style.transform = `translateX(${updateStyle(h,4*viewportHeight,6*viewportHeight,200,0,'%')})`;
-    year2.style.transform = `translateX(${updateStyle(h,6*viewportHeight,6.5*viewportHeight,-200,0,'%')})`;
-    info2.style.transform = `translateX(${updateStyle(h,6*viewportHeight,6.5*viewportHeight,200,0,'%')})`;
-    credit.style.opacity = updateStyle(h,8*viewportHeight,9*viewportHeight,0,50,'%');
+    year1.style.transform = `translateX(${updateStyle(h,4*viewportHeight,6*viewportHeight,-100,0,'%')})`;
+    info1.style.transform = `translateX(${updateStyle(h,4*viewportHeight,6*viewportHeight,100,0,'%')})`;
+    year2.style.transform = `translateX(${updateStyle(h,6*viewportHeight,6.5*viewportHeight,-100,0,'%')})`;
+    info2.style.transform = `translateX(${updateStyle(h,6*viewportHeight,6.5*viewportHeight,100,0,'%')})`;
     Icon1.style.opacity = updateStyle(h,7.5*viewportHeight,8*viewportHeight,0,50,'%');
     Icon2.style.opacity = updateStyle(h,7.5*viewportHeight,8*viewportHeight,0,50,'%');
     Icon3.style.opacity = updateStyle(h,7.5*viewportHeight,8*viewportHeight,0,50,'%');
     Icon4.style.opacity = updateStyle(h,7.5*viewportHeight,8*viewportHeight,0,50,'%');
+    title.style.opacity = updateStyle(h,11*viewportHeight,12*viewportHeight,0,100,'%');
+    message1.style.opacity = updateStyle(h,12*viewportHeight,13.5*viewportHeight,0,100,'%');
     fixedElement(h,viewportHeight,d2,100,'vh');
     fixedElement(h,6*viewportHeight,d3,600,'vh');
     fixedElement(h,8*viewportHeight,d4,800,'vh');
+    fixedElement(h,10*viewportHeight,d5,1000,'vh');
+    fixedElement(h,12*viewportHeight,d6,1200,'vh');
 });
 //anime update
 function updateStyle(h,start,end,valueStart,valueEnd,suffix){
@@ -73,6 +81,5 @@ function fixedElement(h,animeStart,element,elementTop,suffix){
         element.style.position = 'fixed';
         element.style.top = '0';
     }
-    console.log(animeStart)
 }
 
